0e00a88d99a9d9075367bdef9c8f4a320d28a0e1
guptamanas149@gmail.com <guptamanas149@gmail.com>
2022-03-22 00:31:21 +0530

    changed msg and styles

--- src/animations.js
+++ src/animations.js
@@ -1,13 +1,12 @@
-function animate(){
-    gsap.from('.left_to_right',
-    {
-        duration:2,
-        x:'-100vw',
-        ease:'power2.in',
-        repeat:-1,
-        stagger:.4,
-        repeatDelay:2
-    })
+function animate() {
+  gsap.from(".left_to_right", {
+    duration: 2,
+    x: "-100vw",
+    ease: "power2.in",
+    repeat: -1,
+    stagger: 0.4,
+    repeatDelay: 2,
+  });
 }
 
-export {animate};
+export { animate };
