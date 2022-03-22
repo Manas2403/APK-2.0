893b4714b8b4c384e1c2a50696aa1fcbc161a6f8
guptamanas149@gmail.com <guptamanas149@gmail.com>
2022-03-22 21:27:19 +0530

    changed navbar styling added fonts and much more

--- src/menu.js
+++ src/menu.js
@@ -7,7 +7,8 @@ const nav = document.querySelector(".header__navbar");
 const menuNav = document.querySelector(".header__navbar__navlinks");
 
 const navItems = document.querySelectorAll(".header__navbar__navlink");
-
+const mainParent = document.querySelector(".mainParent");
+const footer = document.querySelector("footer");
 let showMenu = false;
 
 menuBtn.addEventListener("click", () => {
@@ -15,15 +16,17 @@ menuBtn.addEventListener("click", () => {
     hamburger.classList.add("open");
 
     nav.classList.add("open");
-
     menuNav.classList.add("open");
-
+    mainParent.style.display = "none";
+    menuNav.style.opacity = 0.6;
+    footer.style.display = "none";
     showMenu = true;
   } else {
     hamburger.classList.remove("open");
     nav.classList.remove("open");
     menuNav.classList.remove("open");
-
+    mainParent.style.display = "block";
+    footer.style.display = "block";
     showMenu = false;
   }
 });
